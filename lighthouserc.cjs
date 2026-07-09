50080ca232e1e8cba1d3f9b6659bd0e605dac28d
axu1-eng <axu1@haverford.edu>
2026-07-09 15:49:06 -0400

    Added most relevant URLs to check

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -2,6 +2,20 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './dist',
+
+      url: [
+        "/",
+        "/index/",
+        "/404/",
+        "/explore/documents/",
+        "/explore/editions/",
+        "/explore/people/",
+        "/explore/towns/",
+        "/get-started/guides",
+        "/get-started/guides/finding-documents/",
+        "/get-started/guides/linguists/",
+        "/document/Al641",
+      ]
     },
     upload: {
       target: 'temporary-public-storage',
